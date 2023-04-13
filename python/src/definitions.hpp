53091f733e251a3a0822a59666857431800841a8
Wim Haeck <whaeck@gmail.com>
2023-04-13 15:44:57 -0600

    Adding Python bindings for the LegendreSeries function

--- python/src/definitions.hpp
+++ python/src/definitions.hpp
@@ -37,6 +37,70 @@ void addStandardDomainDefinitions( PythonClass& component ) {
     "is_contained",
     &Component::isContained,
     python::arg( "x" ),
+    "Check whether or not a value is inside the domain (excluding boundaries)\n\n"
+    "Arguments:\n"
+    "    self   the domain\n"
+    "    x      the value to be tested"
+  );
+}
+
+/**
+ *  @brief Add standard function definitions
+ *
+ *  This adds the following standard functions:
+ *    in_inside, is_contained, __call__, evaluate, domain
+ *
+ *  @param[in] component   the domain to which the definitions have to be added
+ */
+template < typename Component, typename X, typename Y, typename PythonClass >
+void addStandardFunctionDefinitions( PythonClass& component ) {
+
+  component
+  .def_property_readonly(
+
+    "domain",
+    [] ( const Component& self ) -> decltype(auto)
+       { return self.domain(); },
+    "The Legendre coefficients"
+  )
+  .def(
+
+    "evaluate",
+    &Component::evaluate,
+    python::arg( "x" ),
+    "Evaluate the function for a value of x\n\n"
+    "Arguments:\n"
+    "    self   the domain\n"
+    "    x      the value to be evaluated"
+  )
+  .def(
+
+    "__call__",
+    [] ( const Component& self, const X& x ) -> Y
+       { return self( x ); },
+    python::arg( "x" ),
+    "Evaluate the function for a value of x\n\n"
+    "Arguments:\n"
+    "    self   the domain\n"
+    "    x      the value to be evaluated"
+  )
+  .def(
+
+    "is_inside",
+    [] ( const Component& self, const X& x ) -> bool
+       { return self.isInside( x ); },
+    python::arg( "x" ),
+    "Check whether or not a value is inside the domain (including boundaries)\n\n"
+    "Arguments:\n"
+    "    self   the domain\n"
+    "    x      the value to be tested"
+  )
+  .def(
+
+    "is_contained",
+    [] ( const Component& self, const X& x ) -> bool
+       { return self.isContained( x ); },
+    python::arg( "x" ),
     "Check whether or not a value is inside the domain (including boundaries)\n\n"
     "Arguments:\n"
     "    self   the domain\n"
