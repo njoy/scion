d9a0a990363754e05a4dff74a5af030348c5d074
Wim Haeck <whaeck@gmail.com>
2023-04-14 16:19:47 -0600

    Adding the derivative of the Legendre series

---
+++ src/scion/math/LegendreSeries/src/derivative.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @brief Get the derivative of the Legendre series
+ *
+ *  The derivative function of a Legendre series function is another
+ *  Legendre series function. The coefficients of the new Legendre series
+ *  are calculated using the derivative of a Legendre polynomial as a
+ *  function of other Legendre polynomials:
+ *    d/dx P_(n + 1) = (2 * n + 1) * P_n + (2 * (n - 2) + 1) * P_(n - 2)
+ *  knowing that:
+ *    d/dx P_0 = 0.0
+ *    d/dx P_1 = P_0
+ *
+ *  This formula can be derived using the following property of Legendre
+ *  polynomials:
+ *    ( 2 * n + 1 ) * P_n = d/dx P_(n + 1) - d/dx P_(n - 1)
+ *
+ *  The derivative function is defined over the same domain as the
+ *  original function.
+ */
+LegendreSeries derivative() const {
+
+  const unsigned int order = this->order();
+  if ( 0 == order ) {
+
+    return LegendreSeries( { Y( 0. ) } );
+  }
+
+  std::vector< Y > derivative( order, Y( 0. ) );
+  for ( unsigned int i = 0; i < order; ++i ) {
+
+    const Y a = this->coefficients()[i + 1];
+    int j = static_cast< int >( i );
+    while ( 0 <= j ) {
+
+      derivative[j] += static_cast< Y >( 2 * j + 1 ) * a;
+      j -= 2;
+    }
+  }
+
+  return LegendreSeries( std::move( derivative ) );
+}
--- src/scion/math/LegendreSeries/test/LegendreSeries.test.cpp
+++ src/scion/math/LegendreSeries/test/LegendreSeries.test.cpp
@@ -31,6 +31,9 @@ SCENARIO( "LegendreSeries" ) {
         CHECK( 4. == Approx( chunk.coefficients()[3] ) );
 
         CHECK( true == std::holds_alternative< IntervalDomain< double > >( chunk.domain() ) );
+      } // THEN
+
+      THEN( "a LegendreSeries can be evaluated" ) {
 
         CHECK( -0.5 == Approx( chunk.evaluate(  0. ) ) );
         CHECK( 10.0 == Approx( chunk.evaluate(  1. ) ) );
@@ -39,6 +42,33 @@ SCENARIO( "LegendreSeries" ) {
         CHECK( -0.5 == Approx( chunk(  0. ) ) );
         CHECK( 10.0 == Approx( chunk(  1. ) ) );
         CHECK( -2.0 == Approx( chunk( -1. ) ) );
+      } // THEN
+
+      THEN( "a LegendreSeries can be differentiated" ) {
+
+        LegendreSeries< double > first = chunk.derivative();
+        LegendreSeries< double > second = first.derivative();
+        LegendreSeries< double > third = second.derivative();
+        LegendreSeries< double > fourth = third.derivative();
+
+        CHECK( 2 == first.order() );
+        CHECK( 3 == first.coefficients().size() );
+        CHECK( 6. == Approx( first.coefficients()[0] ) );
+        CHECK( 9. == Approx( first.coefficients()[1] ) );
+        CHECK( 20. == Approx( first.coefficients()[2] ) );
+
+        CHECK( 1 == second.order() );
+        CHECK( 2 == second.coefficients().size() );
+        CHECK( 9. == Approx( second.coefficients()[0] ) );
+        CHECK( 60. == Approx( second.coefficients()[1] ) );
+
+        CHECK( 0 == third.order() );
+        CHECK( 1 == third.coefficients().size() );
+        CHECK( 60. == Approx( third.coefficients()[0] ) );
+
+        CHECK( 0 == fourth.order() );
+        CHECK( 1 == fourth.coefficients().size() );
+        CHECK( 0. == Approx( fourth.coefficients()[0] ) );
       } // THEN
     } // WHEN
   } // GIVEN
