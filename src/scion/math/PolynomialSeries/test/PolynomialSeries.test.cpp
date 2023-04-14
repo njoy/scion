0e133b809901e4067976f8fd942eb4197a60214b
Wim Haeck <whaeck@gmail.com>
2023-04-14 14:32:27 -0600

    Adding the derivative to the PolynomialSeries

--- src/scion/math/PolynomialSeries/test/PolynomialSeries.test.cpp
+++ src/scion/math/PolynomialSeries/test/PolynomialSeries.test.cpp
@@ -33,6 +33,9 @@ SCENARIO( "PolynomialSeries" ) {
         CHECK( 4. == Approx( chunk.coefficients()[3] ) );
 
         CHECK( true == std::holds_alternative< IntervalDomain< double > >( chunk.domain() ) );
+      } // THEN
+
+      THEN( "a PolynomialSeries can be evaluated" ) {
 
         CHECK(  1.0 == Approx( chunk.evaluate(  0. ) ) );
         CHECK( 10.0 == Approx( chunk.evaluate(  1. ) ) );
@@ -41,6 +44,45 @@ SCENARIO( "PolynomialSeries" ) {
         CHECK(  1.0 == Approx( chunk(  0. ) ) );
         CHECK( 10.0 == Approx( chunk(  1. ) ) );
         CHECK( -2.0 == Approx( chunk( -1. ) ) );
+      } // THEN
+
+      THEN( "a PolynomialSeries can be differentiated" ) {
+
+        PolynomialSeries< double > first = chunk.derivative();
+        PolynomialSeries< double > second = first.derivative();
+        PolynomialSeries< double > third = second.derivative();
+        PolynomialSeries< double > fourth = third.derivative();
+
+        CHECK( 2 == first.order() );
+        CHECK( 3 == first.coefficients().size() );
+        CHECK( 2. == Approx( first.coefficients()[0] ) );
+        CHECK( 6. == Approx( first.coefficients()[1] ) );
+        CHECK( 12. == Approx( first.coefficients()[2] ) );
+        IntervalDomain< double > domain = std::get< IntervalDomain< double > >( first.domain() );
+        CHECK( -1. == Approx( domain.lowerLimit() ) );
+        CHECK(  1. == Approx( domain.upperLimit() ) );
+
+        CHECK( 1 == second.order() );
+        CHECK( 2 == second.coefficients().size() );
+        CHECK( 6. == Approx( second.coefficients()[0] ) );
+        CHECK( 24. == Approx( second.coefficients()[1] ) );
+        domain = std::get< IntervalDomain< double > >( second.domain() );
+        CHECK( -1. == Approx( domain.lowerLimit() ) );
+        CHECK(  1. == Approx( domain.upperLimit() ) );
+
+        CHECK( 0 == third.order() );
+        CHECK( 1 == third.coefficients().size() );
+        CHECK( 24. == Approx( third.coefficients()[0] ) );
+        domain = std::get< IntervalDomain< double > >( third.domain() );
+        CHECK( -1. == Approx( domain.lowerLimit() ) );
+        CHECK(  1. == Approx( domain.upperLimit() ) );
+
+        CHECK( 0 == fourth.order() );
+        CHECK( 1 == fourth.coefficients().size() );
+        CHECK( 0. == Approx( fourth.coefficients()[0] ) );
+        domain = std::get< IntervalDomain< double > >( fourth.domain() );
+        CHECK( -1. == Approx( domain.lowerLimit() ) );
+        CHECK(  1. == Approx( domain.upperLimit() ) );
       } // THEN
     } // WHEN
   } // GIVEN
