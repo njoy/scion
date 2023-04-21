856062c58b60cedda9d80a19cba4b041cc0c8e59
Wim Haeck <whaeck@gmail.com>
2023-04-21 12:12:51 -0600

    Returning only the real roots and make them sorted

--- src/scion/math/LegendreSeries/src/roots.hpp
+++ src/scion/math/LegendreSeries/src/roots.hpp
@@ -1,17 +1,20 @@
 /**
  *  @brief Calculate the roots of the Legendre series so that f(x) = a
+
+ *  This function calculates all roots on the real axis of the Legendre series.
  *
- *  This function calculates all roots in the complex plane of the polynomial
- *  series. The roots of the polynomial series are the eigenvalues of the
- *  Frobenius companion matrix whose elements are trivial functions of the
- *  coefficients of the polynomial series. The roots on the real axis are then
- *  improved upon using a few iterations of the Newton-Rhapson method.
+ *  The roots of the Legendre series are the eigenvalues of the Frobenius
+ *  companion matrix whose elements are trivial functions of the coefficients of
+ *  the Legendre series. The resulting roots are in the complex plane so the
+ *  roots that are not on the real axis are filtered out. The roots on the real
+ *  axis are then improved upon using a few iterations of the Newton-Rhapson
+ *  method.
  *
  *  @param[in] a   the value of a (default is zero)
  */
-std::vector< std::complex< X > > roots( const Y& a = Y( 0. ) ) const {
+std::vector< X > roots( const Y& a = Y( 0. ) ) const {
 
-  std::vector< std::complex< X > > roots;
+  std::vector< X > roots;
   roots.reserve( this->order() );
 
   if ( 1 == this->order() ) {
@@ -31,11 +34,10 @@ std::vector< std::complex< X > > roots( const Y& a = Y( 0. ) ) const {
 
         roots.emplace_back( newton( value.real(), functor, derivative ) );
       }
-      else {
-
-        roots.emplace_back( value );
-      }
     }
+
+    std::sort( roots.begin(), roots.end() );
+    roots.erase( std::unique( roots.begin(), roots.end() ), roots.end() );
   }
 
   return roots;
