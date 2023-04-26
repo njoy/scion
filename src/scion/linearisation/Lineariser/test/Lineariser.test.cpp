09357fa1eb099b2fd09baa9c8ded27c7e17ab30e
Wim Haeck <whaeck@gmail.com>
2023-04-26 11:58:46 -0600

    updating Lineariser test

--- src/scion/linearisation/Lineariser/test/Lineariser.test.cpp
+++ src/scion/linearisation/Lineariser/test/Lineariser.test.cpp
@@ -6,6 +6,7 @@
 // other includes
 #include <cmath>
 #include "scion/linearisation/ToleranceConvergence.hpp"
+#include "scion/linearisation/MidpointSplit.hpp"
 
 // convenience typedefs
 using namespace njoy::scion;
@@ -15,16 +16,10 @@ SCENARIO( "Lineariser" ) {
   GIVEN( "valid data for linearisation" ) {
 
     // y = f(x) = exp(x)
-    auto functor = [] ( double x ) {
+    auto functor = [] ( double x ) { return std::exp( x ); };
 
-      return std::exp( x );
-    };
-
-    // take the midpoint between x1 and x2
-    auto midpoint = [] ( double x1, double x2 ) {
-
-      return 0.5 * ( x1 + x2 );
-    };
+    // split a panel using the midpoint
+    linearisation::MidpointSplit< double > split;
 
     // convergence tolerance < 25%
     linearisation::ToleranceConvergence< double > convergence( 0.25 );
@@ -39,7 +34,7 @@ SCENARIO( "Lineariser" ) {
         std::vector< double > y;
 
         linearisation::Lineariser linearise( x, y );
-        linearise( grid.begin(), grid.end(), functor, convergence, midpoint );
+        linearise( grid.begin(), grid.end(), functor, convergence, split );
 
         CHECK( 9 == x.size() );
         CHECK( 9 == y.size() );
@@ -76,7 +71,7 @@ SCENARIO( "Lineariser" ) {
         std::vector< double > y;
 
         linearisation::Lineariser linearise( x, y );
-        linearise( grid.begin(), grid.end(), functor, convergence, midpoint );
+        linearise( grid.begin(), grid.end(), functor, convergence, split );
 
         CHECK( 11 == x.size() );
         CHECK( 11 == y.size() );
