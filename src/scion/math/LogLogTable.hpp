fc45cf96af9fef9b57b7bf98417673d17fff32ea
Wim Haeck <whaeck@gmail.com>
2023-04-27 15:16:34 -0600

    Movign the linearise function to a separate file

--- src/scion/math/LogLogTable.hpp
+++ src/scion/math/LogLogTable.hpp
@@ -68,26 +68,7 @@ namespace math {
       return this->table_.evaluate( x );
     }
 
-    /**
-     *  @brief Linearise the table and return a LinearLinearTable
-     *
-     *  Note: this function returns a copy of the original table.
-     *
-     *  @param[in] convergence    the linearisation convergence criterion (default 0.1 %)
-     */
-    template < typename Convergence = linearisation::ToleranceConvergence< X, Y > >
-    LinearLinearTable< X, Y > linearise( Convergence&& convergence = Convergence() ) const {
-
-      std::vector< X > x;
-      std::vector< Y > y;
-      linearisation::Lineariser lineariser( x, y );
-      lineariser( this->x(),
-                  *this,
-                  std::forward< Convergence >( convergence ),
-                  linearisation::MidpointSplit< X >() );
-
-      return LinearLinearTable< X, Y >( std::move( x ), std::move( y ) );
-    }
+    #include "scion/math/LogLogTable/src/linearise.hpp"
 
     using Parent::domain;
     using Parent::operator();
