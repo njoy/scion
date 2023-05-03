9e81711bb6a4a1c11247add73015ca485214ffdc
Wim Haeck <whaeck@gmail.com>
2023-05-02 18:20:48 -0600

    Adding clenshaw for Chebyshev series

--- src/scion/math/clenshaw/test/clenshaw.test.cpp
+++ src/scion/math/clenshaw/test/clenshaw.test.cpp
@@ -155,4 +155,94 @@ SCENARIO( "clenshaw" ) {
       } // THEN
     } // WHEN
   } // GIVEN
+
+  GIVEN( "valid data for Chebyshev polynomials" ) {
+
+    // Chebyshev coefficients (from lower order to higher order)
+    std::vector< double > order0 = { 1. };
+    std::vector< double > order1 = { 1., 2. };
+    std::vector< double > order2 = { 1., 2., 3. };
+    std::vector< double > order3 = { 1., 2., 3., 4. };
+
+    auto a = [] ( unsigned int k, double x ) -> double {
+
+      return static_cast< double >( 2 ) * x;
+    };
+    auto b = [] ( unsigned int k, double x ) -> double {
+
+      return - static_cast< double >( 1 );
+    };
+
+    WHEN( "the iterators are used" ) {
+
+      THEN( "the Chebyshev polynomials can be evaluated" ) {
+
+        CHECK(  1. == Approx( math::clenshaw( order0.rbegin(), order0.rend(), a, b, 1., 0., 0. ) ) );
+        CHECK(  1. == Approx( math::clenshaw( order1.rbegin(), order1.rend(), a, b, 1., 0., 0. ) ) );
+        CHECK( -2. == Approx( math::clenshaw( order2.rbegin(), order2.rend(), a, b, 1., 0., 0. ) ) );
+        CHECK( -2. == Approx( math::clenshaw( order3.rbegin(), order3.rend(), a, b, 1., 0., 0. ) ) );
+
+        CHECK(  1. == Approx( math::clenshaw( order0.rbegin(), order0.rend(), a, b, 1., 1., 1. ) ) );
+        CHECK(  3. == Approx( math::clenshaw( order1.rbegin(), order1.rend(), a, b, 1., 1., 1. ) ) );
+        CHECK(  6. == Approx( math::clenshaw( order2.rbegin(), order2.rend(), a, b, 1., 1., 1. ) ) );
+        CHECK( 10. == Approx( math::clenshaw( order3.rbegin(), order3.rend(), a, b, 1., 1., 1. ) ) );
+
+        CHECK(  1. == Approx( math::clenshaw( order0.rbegin(), order0.rend(), a, b, 1., -1., -1. ) ) );
+        CHECK( -1. == Approx( math::clenshaw( order1.rbegin(), order1.rend(), a, b, 1., -1., -1. ) ) );
+        CHECK(  2. == Approx( math::clenshaw( order2.rbegin(), order2.rend(), a, b, 1., -1., -1. ) ) );
+        CHECK( -2. == Approx( math::clenshaw( order3.rbegin(), order3.rend(), a, b, 1., -1., -1. ) ) );
+
+        CHECK(   1. == Approx( math::clenshawChebyshev( order0.rbegin(), order0.rend(), 0. ) ) );
+        CHECK(   1. == Approx( math::clenshawChebyshev( order1.rbegin(), order1.rend(), 0. ) ) );
+        CHECK(  -2. == Approx( math::clenshawChebyshev( order2.rbegin(), order2.rend(), 0. ) ) );
+        CHECK(  -2. == Approx( math::clenshawChebyshev( order3.rbegin(), order3.rend(), 0. ) ) );
+
+        CHECK(  1. == Approx( math::clenshawChebyshev( order0.rbegin(), order0.rend(), 1. ) ) );
+        CHECK(  3. == Approx( math::clenshawChebyshev( order1.rbegin(), order1.rend(), 1. ) ) );
+        CHECK(  6. == Approx( math::clenshawChebyshev( order2.rbegin(), order2.rend(), 1. ) ) );
+        CHECK( 10. == Approx( math::clenshawChebyshev( order3.rbegin(), order3.rend(), 1. ) ) );
+
+        CHECK(  1. == Approx( math::clenshawChebyshev( order0.rbegin(), order0.rend(), -1. ) ) );
+        CHECK( -1. == Approx( math::clenshawChebyshev( order1.rbegin(), order1.rend(), -1. ) ) );
+        CHECK(  2. == Approx( math::clenshawChebyshev( order2.rbegin(), order2.rend(), -1. ) ) );
+        CHECK( -2. == Approx( math::clenshawChebyshev( order3.rbegin(), order3.rend(), -1. ) ) );
+      } // THEN
+    } // WHEN
+
+    WHEN( "the vectors are used" ) {
+
+      THEN( "the Chebyshev polynomials can be evaluated" ) {
+
+        CHECK(  1. == Approx( math::clenshaw( order0, a, b, 1., 0., 0. ) ) );
+        CHECK(  1. == Approx( math::clenshaw( order1, a, b, 1., 0., 0. ) ) );
+        CHECK( -2. == Approx( math::clenshaw( order2, a, b, 1., 0., 0. ) ) );
+        CHECK( -2. == Approx( math::clenshaw( order3, a, b, 1., 0., 0. ) ) );
+
+        CHECK(  1. == Approx( math::clenshaw( order0, a, b, 1., 1., 1. ) ) );
+        CHECK(  3. == Approx( math::clenshaw( order1, a, b, 1., 1., 1. ) ) );
+        CHECK(  6. == Approx( math::clenshaw( order2, a, b, 1., 1., 1. ) ) );
+        CHECK( 10. == Approx( math::clenshaw( order3, a, b, 1., 1., 1. ) ) );
+
+        CHECK(  1. == Approx( math::clenshaw( order0, a, b, 1., -1., -1. ) ) );
+        CHECK( -1. == Approx( math::clenshaw( order1, a, b, 1., -1., -1. ) ) );
+        CHECK(  2. == Approx( math::clenshaw( order2, a, b, 1., -1., -1. ) ) );
+        CHECK( -2. == Approx( math::clenshaw( order3, a, b, 1., -1., -1. ) ) );
+
+        CHECK(  1. == Approx( math::clenshawChebyshev( order0, 0. ) ) );
+        CHECK(  1. == Approx( math::clenshawChebyshev( order1, 0. ) ) );
+        CHECK( -2. == Approx( math::clenshawChebyshev( order2, 0. ) ) );
+        CHECK( -2. == Approx( math::clenshawChebyshev( order3, 0. ) ) );
+
+        CHECK(  1. == Approx( math::clenshawChebyshev( order0, 1. ) ) );
+        CHECK(  3. == Approx( math::clenshawChebyshev( order1, 1. ) ) );
+        CHECK(  6. == Approx( math::clenshawChebyshev( order2, 1. ) ) );
+        CHECK( 10. == Approx( math::clenshawChebyshev( order3, 1. ) ) );
+
+        CHECK(  1. == Approx( math::clenshawChebyshev( order0, -1. ) ) );
+        CHECK( -1. == Approx( math::clenshawChebyshev( order1, -1. ) ) );
+        CHECK(  2. == Approx( math::clenshawChebyshev( order2, -1. ) ) );
+        CHECK( -2. == Approx( math::clenshawChebyshev( order3, -1. ) ) );
+      } // THEN
+    } // WHEN
+  } // GIVEN
 } // SCENARIO
