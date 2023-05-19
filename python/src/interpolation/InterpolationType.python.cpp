a2947cd755574e0f82ba7d9189095788becd2adb
Wim Haeck <whaeck@gmail.com>
2023-05-19 06:35:51 -0600

    Adding python bindings

---
+++ python/src/interpolation/InterpolationType.python.cpp
@@ -0,0 +1,33 @@
+// system includes
+#include <pybind11/pybind11.h>
+
+// local includes
+#include "scion/interpolation/InterpolationType.hpp"
+
+// namespace aliases
+namespace python = pybind11;
+
+void wrapInterpolationType( python::module& module ) {
+
+  // type aliases
+  using Component = njoy::scion::interpolation::InterpolationType;
+
+  // wrap views created by this component
+
+  // create the component
+  python::enum_< Component > component(
+
+    module,
+    "InterpolationType",
+    "The interpolation types",
+    python::arithmetic()
+  );
+
+  // wrap the component
+  component
+  .value( "Constant", Component::Constant )
+  .value( "LinearLinear", Component::LinearLinear )
+  .value( "LinearLog", Component::LinearLog )
+  .value( "LogLinear", Component::LogLinear )
+  .value( "LogLog", Component::LogLog );
+}
