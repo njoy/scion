a1c2e2e7f3f75aa44aa105661648b6a5edfe5415
Wim Haeck <whaeck@gmail.com>
2023-05-25 04:11:14 -0600

    Updating unit tests

--- python/test/math/Test_scion_math_LinearLinearTable.py
+++ python/test/math/Test_scion_math_LinearLinearTable.py
@@ -18,6 +18,7 @@ class Test_scion_math_LinearLinearTable( unittest.TestCase ) :
 
             # verify content
             self.assertEqual( InterpolationType.LinearLinear, chunk.interpolation )
+            self.assertEqual( 4, chunk.number_points )
             self.assertEqual( 4, len( chunk.x ) )
             self.assertEqual( 4, len( chunk.y ) )
             self.assertAlmostEqual( 1., chunk.x[0] )
