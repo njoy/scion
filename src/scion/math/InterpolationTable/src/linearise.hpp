ee914b06d77878f83b0608f342cad72f1441d69d
Wim Haeck <whaeck@gmail.com>
2023-05-31 10:22:11 -0600

    Updating the linearise function of the InterpolationTable

--- src/scion/math/InterpolationTable/src/linearise.hpp
+++ src/scion/math/InterpolationTable/src/linearise.hpp
@@ -4,46 +4,53 @@
  *  @param[in] convergence    the linearisation convergence criterion (default 0.1 %)
  */
 template < typename Convergence = linearisation::ToleranceConvergence< X, Y > >
-LinearLinearTable< X, Y > linearise( Convergence&& convergence = Convergence() ) const {
+InterpolationTable< X, Y > 
+linearise( Convergence&& convergence = Convergence() ) const {
+
+  std::vector< X > x;
+  std::vector< Y > y;
+  std::vector< std::size_t > boundaries;
+  std::vector< interpolation::InterpolationType > interpolants;
 
   auto linearise = [&convergence] ( const auto& table )
                                   { return table.linearise( convergence ); };
 
-  if ( this->numberRegions() > 1 ) {
+  auto check = [this] ( const auto& table ) {
 
-    std::vector< X > x;
-    std::vector< Y > y;
+    if ( table.x().end() != this->x().end() ) {
 
-    auto check = [this] ( const auto& table ) {
+      if ( *( table.x().end() ) == table.x().back() ) {
 
-      if ( table.x().end() != this->x().end() ) {
-        if ( *( table.x().end() ) == table.x().back() ) {
-          if ( *( table.y().end() ) != table.y().back() ) {
-            return true;
-          }
+        if ( *( table.y().end() ) != table.y().back() ) {
+
+          return true;
         }
-        return false;
       }
-      return true;
-    };
-
-    for ( const auto& table : this->tables_ ) {
-
-      auto linearised = std::visit( linearise, table );
-      bool isJump = std::visit( check, table );
-
-      x.insert( x.end(),
-                linearised.x().begin(),
-                isJump ? linearised.x().end() : std::prev( linearised.x().end() ) );
-      y.insert( y.end(),
-                linearised.y().begin(),
-                isJump ? linearised.y().end() : std::prev( linearised.y().end() ) );
+      return false;
     }
+    return true;
+  };
 
-    return LinearLinearTable< X, Y >( std::move( x ), std::move( y ) );
-  }
-  else {
+  for ( const auto& table : this->tables_ ) {
 
-    return std::visit( linearise, this->tables_.front() );
+    auto linearised = std::visit( linearise, table );
+    bool isJumpOrEnd = std::visit( check, table );
+
+    if ( isJumpOrEnd ) {
+
+      x.insert( x.end(), linearised.first.begin(), linearised.first.end() );
+      y.insert( y.end(), linearised.second.begin(), linearised.second.end() );
+      boundaries.push_back( x.size() - 1 );
+      interpolants.push_back( interpolation::InterpolationType::LinearLinear );
+    }
+    else {
+
+      x.insert( x.end(), linearised.first.begin(), std::prev( linearised.first.end() ) );
+      y.insert( y.end(), linearised.second.begin(), std::prev( linearised.second.end() ) );
+    }
   }
+
+  return InterpolationTable< X, Y >( std::move( x ), std::move( y ),
+                                     std::move( boundaries ),
+                                     std::move( interpolants ) );
 }
