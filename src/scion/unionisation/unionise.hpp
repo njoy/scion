5f19f348d6bb3d9c21e3d75ad3387352b64fd95c
Wim Haeck <whaeck@gmail.com>
2023-06-22 09:02:37 -0600

    Small modification to unionise

--- src/scion/unionisation/unionise.hpp
+++ src/scion/unionisation/unionise.hpp
@@ -13,7 +13,9 @@ namespace unionisation {
   /**
    *  @brief Unionise two grids and preserve duplicate points that appear in each
    *
-   *  Guess what: the STL library provides exactly what we need :-)
+   *  If the grids do not have the same end point, a duplicate point is inserted
+   *  into the grid corresponding to the lowest end point (unless that is
+   *  already a duplicate point).
    *
    *  @param first        the first grid (assumed to be sorted)
    *  @param second       the second grid (assumed to be sorted)
@@ -22,11 +24,24 @@ namespace unionisation {
   std::vector< X >
   unionise( const std::vector< X >& first, const std::vector< X >& second ) {
 
+    // merge the two grids - keeping a maximum of 2 duplicates
     std::vector< X > grid( first.size() + second.size() );
     auto end = std::set_union( first.begin(), first.end(),
                                second.begin(), second.end(),
                                grid.begin() );
     grid.erase( end, grid.end() );
+
+    // special case: the end points are not the same
+    if ( first.back() != second.back() ) {
+
+      X duplicate = first.back() < second.back() ? first.back() : second.back();
+      auto iter = std::lower_bound( grid.begin(), grid.end(), duplicate );
+      if ( *std::next( iter ) != duplicate ) {
+
+        grid.insert( iter, duplicate );
+      }
+    }
+
     return grid;
   }
 
