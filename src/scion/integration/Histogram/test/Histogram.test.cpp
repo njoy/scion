50aeca7bf2205d4b0783ec67f4725bd1ea5e152d
Wim Haeck <whaeck@gmail.com>
2023-06-26 11:45:36 -0600

    Implementing histogram integration

---
+++ src/scion/integration/Histogram/test/Histogram.test.cpp
@@ -0,0 +1,49 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+using Catch::Matchers::WithinRel;
+
+// what we are testing
+#include "scion/integration/Histogram.hpp"
+
+// other includes
+
+// convenience typedefs
+using namespace njoy::scion;
+
+SCENARIO( "Histogram" ) {
+
+  GIVEN( "Histogram integration object" ) {
+
+    WHEN( "interpolating an interval" ) {
+
+      integration::Histogram integrator{};
+
+      THEN( "the integration is performed correctly" ) {
+
+        double xLeft = 1.0;
+        double xRight = 2.0;
+        double yLeft = 1.0;
+        double yRight = 4.0;
+
+        CHECK_THAT( 1., WithinRel( integrator( xLeft, xRight, yLeft, yRight ) ) );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "histogram integration function" ) {
+
+    WHEN( "interpolating an interval" ) {
+
+      THEN( "the integration is performed correctly" ) {
+
+        double xLeft = 1.0;
+        double xRight = 2.0;
+        double yLeft = 1.0;
+        double yRight = 4.0;
+
+        CHECK_THAT( 1., WithinRel( integration::histogram( xLeft, xRight, yLeft, yRight ) ) );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+} // SCENARIO
