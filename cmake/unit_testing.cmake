64e69a925d5fc489b72a58b4f91af530123dc6fb
Wim Haeck <whaeck@gmail.com>
2025-06-27 04:31:54 -0600

    deleting the unionise function

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -32,7 +32,6 @@ add_subdirectory( src/utility/IteratorView/test )
 
 add_subdirectory( src/scion/verification/ranges/test )
 
-add_subdirectory( src/scion/unionisation/unionise/test )
 add_subdirectory( src/scion/unionisation/Unioniser/test )
 
 add_subdirectory( src/scion/integration/Histogram/test )
