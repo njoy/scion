68d7b35b5e407fe9640a21cf562a722ab0cf94bc
Wim Haeck <whaeck@gmail.com>
2026-04-08 12:58:52 -0600

    updating tools and catch to remove another compiler warning

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,12 +4,12 @@ include( shacl_FetchContent )
 
 shacl_FetchContent_Declare( tools
     GIT_REPOSITORY  ../../njoy/tools
-    GIT_TAG         eec23bc922d778e3de3f14c7fd40482d9c3bd636  # NOT A RELEASED VERSION - CHANGE ME!
+    GIT_TAG         9428024113dd96ffb3e2fe87e3bdfec9aa225a32  # NOT A RELEASED VERSION - CHANGE ME!
     )
 
 shacl_FetchContent_Declare( eigen
     GIT_REPOSITORY  ../../njoy/eigen.git
-    GIT_TAG         bc3b39870ecb690a623a3f49149a358b95c5781d # tag: 5.0.1
+    GIT_TAG         bc3b39870ecb690a623a3f49149a358b95c5781d  # tag: 5.0.1
     )
 
 #######################################################################
@@ -19,7 +19,7 @@ shacl_FetchContent_Declare( eigen
 if(scion.python)
   shacl_FetchContent_Declare( pybind11
       GIT_REPOSITORY  ../../pybind/pybind11
-      GIT_TAG         a2e59f0e7065404b44dfe92a28aca47ba1378dc4 # tag: v2.13.6
+      GIT_TAG         a2e59f0e7065404b44dfe92a28aca47ba1378dc4  # tag: v2.13.6
       )
   shacl_FetchContent_MakeAvailable(
     pybind11
@@ -29,7 +29,7 @@ endif()
 if(scion.tests)
   shacl_FetchContent_Declare( Catch2
       GIT_REPOSITORY  ../../catchorg/Catch2
-      GIT_TAG         3f0283de7a9c43200033da996ff9093be3ac84dc # tag: v3.3.2
+      GIT_TAG         b670de4fe12ac7c5e858b7de3a14fb4bd18c760e  # tag: v3.14.0
       )
   shacl_FetchContent_MakeAvailable(
       Catch2
