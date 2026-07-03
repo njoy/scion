ffb5c1bbc1bfceb4434af20ff6a661b072131f46
Wim Haeck <whaeck@gmail.com>
2026-07-03 09:09:55 -0600

    updating test

--- test/scion/math/GroupedTable.test.cpp
+++ test/scion/math/GroupedTable.test.cpp
@@ -10,96 +10,91 @@ using Catch::Matchers::WithinRel;
 
 // convenience typedefs
 using namespace njoy::scion;
-template < typename X, typename Y >
-using GroupedTable = math::GroupedTable< X, Y >;
+template < typename X, typename values >
+using GroupedTable = math::GroupedTable< X, values >;
 
 SCENARIO( "GroupedTable" ) {
 
-    GIVEN( "tabulated double x,y data" ) {
+  GIVEN( "tabulated double x,y data" ) {
 
-        std::vector< double > bounds = { 0., 10., 25., 32. };
-        std::vector< double > y = { 34., 35., 36. };
+    std::vector< double > boundaries = { 0., 10., 25., 32. };
+    std::vector< double > values = { 34., 35., 36. };
 
-        WHEN( " the ref is given as a reference_wrapper ") {
+    WHEN( " the ref is given as a reference_wrapper ") {
 
+      GroupedTable< double, double > table( boundaries, values );
 
-            GroupedTable< double, double > table( bounds, y );
+      THEN( "a GroupedTable can be constructed and members can be tested" ) {
 
-            THEN( "a GroupedTable can be constructed and members can be tested" ) {
+        CHECK( table.values().size() == 3 );
+        CHECK( table.boundaries().size() == 4 );
+        CHECK( table.numberGroups() == 3 );
 
-                CHECK( table.values().size() == 3 );
-                CHECK( table.boundaries().size() == 4 );
-                CHECK( table.numberGroups() == 3 );
+        for ( unsigned int i = 0; i < table.numberGroups(); i++ ) {
 
-                for ( unsigned int i = 0; i < table.numberGroups(); i++ ) {
+          CHECK_THAT( table.values()[i], WithinRel( values[i] ) );
+        }
 
-                    CHECK_THAT( table.values()[i], WithinRel( y[i] ) );
-                }
+        for ( unsigned int i = 0; i <= table.numberGroups(); i++ ) {
 
-                for ( unsigned int i = 0; i <= table.numberGroups(); i++ ) {
+          CHECK_THAT( table.boundaries()[i], WithinRel( boundaries[i] ) );
+        }
+      } // then
 
-                    CHECK_THAT( table.boundaries()[i], WithinRel( bounds[i] ) );
-                }
+      THEN( "a GroupedTable can be multiplied" ) {
 
-            } // then
+        GroupedTable< double, double > table( boundaries, values );
+        GroupedTable< double, double > changed = 2. * table;
 
-            THEN( "a GroupedTable can be multiplied" ) {
+        CHECK( changed.values().size() == 3 );
+        CHECK( changed.boundaries().size() == 4 );
+        CHECK( changed.numberGroups() == 3 );
 
-                GroupedTable< double, double > table( bounds, y );
-                GroupedTable< double, double > changed = 2. * table;
+        for ( unsigned int i = 0; i < table.numberGroups(); i++ ) {
 
-                CHECK( changed.values().size() == 3 );
-                CHECK( changed.boundaries().size() == 4 );
-                CHECK( changed.numberGroups() == 3 );
+            CHECK_THAT( changed.values()[i], WithinRel( 2. * values[i] ) );
+        }
 
-                for ( unsigned int i = 0; i < table.numberGroups(); i++ ) {
+        for ( unsigned int i = 0; i <= table.numberGroups(); i++ ) {
 
-                    CHECK_THAT( changed.values()[i], WithinRel( 2. * y[i] ) );
-                }
+            CHECK_THAT( changed.boundaries()[i], WithinRel( boundaries[i] ) );
+        }
 
-                for ( unsigned int i = 0; i <= table.numberGroups(); i++ ) {
+        GroupedTable< double, double > changedBack = changed / 2.;
 
-                    CHECK_THAT( changed.boundaries()[i], WithinRel( bounds[i] ) );
-                }
+        CHECK( changedBack.values().size() == 3 );
+        CHECK( changedBack.boundaries().size() == 4 );
+        CHECK( changedBack.numberGroups() == 3 );
 
+        for ( unsigned int i = 0; i < table.numberGroups(); i++ ) {
 
-                GroupedTable< double, double > changedBack = changed / 2.;
+            CHECK_THAT( changedBack.values()[i], WithinRel( values[i] ) );
+        }
 
-                CHECK( changedBack.values().size() == 3 );
-                CHECK( changedBack.boundaries().size() == 4 );
-                CHECK( changedBack.numberGroups() == 3 );
+        for ( unsigned int i = 0; i <= table.numberGroups(); i++ ) {
 
-                for ( unsigned int i = 0; i < table.numberGroups(); i++ ) {
+            CHECK_THAT( changedBack.boundaries()[i], WithinRel( boundaries[i] ) );
+        }
 
-                    CHECK_THAT( changedBack.values()[i], WithinRel( y[i] ) );
-                }
+        CHECK( changedBack == table );
+        CHECK( changed != table );
+      } // then
+    } // when
 
-                for ( unsigned int i = 0; i <= table.numberGroups(); i++ ) {
+    WHEN( "bad values are given" ) {
 
-                    CHECK_THAT( changedBack.boundaries()[i], WithinRel( bounds[i] ) );
-                }
+      std::vector< double > outOfOrder{ 1., 2., 1.5, 6. };
+      std::vector< double > notUnique{ 1., 3., 3., 5. };
+      std::vector< double > notEnough{ 1. };
+      std::vector< double > wrongSize{ 1., 2. };
 
-                CHECK( changedBack == table );
-                CHECK( changed != table );
+      THEN( "exceptions are thrown" ) {
 
-            } // then
-        }  // when
-
-        WHEN( "bad values are given" ) {
-
-            std::vector< double > outOfOrder{ 1., 2., 1.5, 6. };
-            std::vector< double > notUnique{ 1., 3., 3., 5. };
-            std::vector< double > notEnough{ 1. };
-            std::vector< double > wrongSize{ 1., 2. };
-
-            THEN( "exceptions are thrown" ) {
-
-                CHECK_THROWS( GroupedTable< double, double >( outOfOrder, y ) );
-                CHECK_THROWS( GroupedTable< double, double >( notUnique, y ) );
-                CHECK_THROWS( GroupedTable< double, double >( notEnough, y ) );
-                CHECK_THROWS( GroupedTable< double, double >( wrongSize, y ) );
-            } // then
-        } // when
-
-    } // given
+        CHECK_THROWS( GroupedTable< double, double >( outOfOrder, values ) );
+        CHECK_THROWS( GroupedTable< double, double >( notUnique, values ) );
+        CHECK_THROWS( GroupedTable< double, double >( notEnough, values ) );
+        CHECK_THROWS( GroupedTable< double, double >( wrongSize, values ) );
+      } // then
+    } // when
+  } // given
 } // scenario
